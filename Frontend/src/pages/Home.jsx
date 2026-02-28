a9ff464694e824143b776ea7058953cb20baf419
Tamilvani <limatinav2005@gmail.com>
2026-02-28 13:37:51 +0530

    final login page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,10 +3,6 @@ import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap, LayersControl } from "react-leaflet";
 import { useLocation } from "./useLocation"; 
 
-// src/pages/Home.jsx
-import React, { useState, useEffect, useRef } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
-
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
